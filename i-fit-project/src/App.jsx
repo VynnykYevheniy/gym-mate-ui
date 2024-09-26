837036737af5bf09a6036e751bf0b680ff5ce41c
Yevheniy Vynnyk <vinnik27734@gmail.com>
2024-09-27 00:14:34 +0300

    Testing lib telegram-web-app

--- i-fit-project/src/App.jsx
+++ i-fit-project/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Header from './components/Header';
 import './App.css';
