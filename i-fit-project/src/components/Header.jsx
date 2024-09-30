c86386ea17c6657374f46a8f6b052f94282ec8a8
Yevheniy Vynnyk <vinnik27734@gmail.com>
2024-09-30 17:58:24 +0300

    resolve adaptation

--- i-fit-project/src/components/Header.jsx
+++ i-fit-project/src/components/Header.jsx
@@ -6,18 +6,17 @@ export default function Header() {
 	const {token} = useToken();
 
 	return (
-		<header className="flex items-center justify-between p-6 bg-white-600 shadow">
+		<header className="flex items-center justify-between p-6 bg-white shadow">
 			<Link to="/" className="flex items-center gap-2">
 				<div className="h-10 w-10 bg-[url('./assets/biceps.svg')] bg-no-repeat bg-contain"/>
 				<span className="text-xl font-black">Gym
-						<span className="text-green-600"> Mate</span>
-					</span>
+            <span className="text-green-600"> Mate</span>
+        </span>
 			</Link>
-
 
 			{!token ? (
 				<nav>
-					<ul className="list-none inline-flex space-x-2">
+					<ul className="list-none flex space-x-2">
 						<li>
 							<Link
 								className="nav-link scrollto rounded-md bg-green-600 py-2 px-4 font-semibold text-white shadow-lg transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-xl focus:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
