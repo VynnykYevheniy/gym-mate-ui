a0c14796989e9f9a3b78eb6647d8f328b92feb74
Yevheniy Vynnyk <vinnik27734@gmail.com>
2024-09-30 17:28:25 +0300

    fix: resolve sourcemap error

--- i-fit-project/src/App.jsx
+++ i-fit-project/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import Home from "./pages/Home";
 import Header from './components/Header';
 import './App.css';
+import Home from "./pages/Home";
 import Register from './pages/Register';
 import Login from './pages/Login';
 import TelegramWebApp from './components/TelegramWebApp.jsx';
@@ -12,12 +12,12 @@ function App() {
 	return (
 		<TokenProvider>
 			<Header/>
-				<Routes>
-					<Route path="/" element={<Home/>}/>
-					<Route path="/signup" element={<Register/>}/>
-					<Route path="/signin" element={<Login/>}/>
-					<Route path="/telegram" element={<TelegramWebApp/>}/> {/* Новый маршрут */}
-				</Routes>
+			<Routes>
+				<Route path="/" element={<Home/>}/>
+				<Route path="/signup" element={<Register/>}/>
+				<Route path="/signin" element={<Login/>}/>
+				<Route path="/telegram" element={<TelegramWebApp/>}/>
+			</Routes>
 		</TokenProvider>
 	);
 }
