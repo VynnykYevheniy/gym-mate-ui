fdda99ef304b67e76f20bcb8722c838c731827d0
Yevheniy Vynnyk <vinnik27734@gmail.com>
2024-11-16 15:27:18 +0200

    update routers

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,34 +1,39 @@
-import {createContext, useEffect, useState} from 'react';
-import {useNavigate} from "react-router-dom";
+import { createContext, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
-export function AuthProvider({children}) {
-	const [token, setToken] = useState(localStorage.getItem('token'));
+export function AuthProvider({ children }) {
+	const [token, setToken] = useState(localStorage.getItem("token"));
 	const profileLink = localStorage.getItem("profileLink");
-	const navigate = useNavigate(); // Hook to programmatically navigate
+	const navigate = useNavigate();
+	const location = useLocation(); // Получаем текущий путь
+
 	useEffect(() => {
-		const savedToken = localStorage.getItem('token');
-		if (savedToken) {
-			setToken(savedToken);
-			navigate(profileLink);
-		} else {
-			navigate('/signin');
+		const savedToken = localStorage.getItem("token");
+		const isAuthPage = ["/signin", "/signup"].includes(location.pathname); // Проверяем, находится ли пользователь на странице аутентификации
+
+		if (!savedToken && !isAuthPage) {
+			// Если токен отсутствует и пользователь не на странице аутентификации, перенаправляем на /signin
+			navigate("/signin");
+		} else if (savedToken && isAuthPage) {
+			// Если токен есть и пользователь на странице аутентификации, перенаправляем на профиль
+			navigate(profileLink || "/profile");
 		}
-	}, [navigate, profileLink, token]);
+	}, [navigate, location.pathname, profileLink]);
 
 	const login = (newToken) => {
-		localStorage.setItem('token', JSON.stringify(newToken));
+		localStorage.setItem("token", JSON.stringify(newToken));
 		setToken(newToken);
 	};
 
 	const logout = () => {
-		localStorage.clear()
+		localStorage.clear();
 		setToken(null);
 	};
 
 	return (
-		<AuthContext.Provider value={{token, login, logout}}>
+		<AuthContext.Provider value={{ token, login, logout }}>
 			{children}
 		</AuthContext.Provider>
 	);
